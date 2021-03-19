b78748d2c248de840f612724e6281123a46e76ef
Dallok <cavok0929@gmail.com>
2021-03-19 23:20:58 +0900

    CSS: Scroll Events

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 
 // Make navbar transparent when it is on the top
 
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView();
+}
+
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -19,10 +25,39 @@ const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
-  if(link == null) {
+  if (link == null) {
     return;
   }
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: "smooth"})
-})
+
+  scrollIntoView(link);
+});
+
+// Handle scrolling when clicking Contact button
+const contactBtn = document.querySelector(".home__contact");
+
+contactBtn.addEventListener("click", event => scrollIntoView("#contact"));
+
+// Fading out the home dashboard scrolling down
+const homeDash = document.querySelector(".home__container");
+const homeHeight = homeDash.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  homeDash.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Arrow-up button show when scrolling down
+const arrowBtn = document.querySelector(".arrow-up");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowBtn.classList.add("visible");
+  } else {
+    arrowBtn.classList.remove("visible");
+  }
+});
+
+arrowBtn.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
+
 
