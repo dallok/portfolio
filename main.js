be86da9bd5732f5026a3b4e4a930b1804d39c052
Dallok <cavok0929@gmail.com>
2021-03-20 16:34:24 +0900

    JS: setting button state

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ arrowBtn.addEventListener("click", () => {
 
 // Projects
 const workBtnContainer = document.querySelector(".work__categories");
-const projectContainer = document.querySelector(".workgit__projects");
+const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
 
 workBtnContainer.addEventListener("click", e => {
@@ -70,9 +70,13 @@ workBtnContainer.addEventListener("click", e => {
     return;
   }
 
-  projectContainer.classList.add("anime-out")
-  
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target = e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+  target.classList.add("selected");
 
+  projectContainer.classList.add("anime-out");
   setTimeout(() => {
     projects.forEach(project => {
       if (filter === "*" || filter === project.dataset.type) {
@@ -81,6 +85,6 @@ workBtnContainer.addEventListener("click", e => {
         project.classList.add("invisible");
       }
     });
-    projectContainer.classList.remove("anime-out")
-  }, 300)
+    projectContainer.classList.remove("anime-out");
+  }, 300);
 });
